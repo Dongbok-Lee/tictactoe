b3aa38d52da706f2ccaaf61df2c80542bed386b5
동복 <typoon0820@gmail.com>
2021-02-24 18:16:33 +0900

    승리 로직 추가

--- tictactoe.js
+++ tictactoe.js
@@ -1,9 +1,12 @@
 var n = 9;
 var numbers = new Array();
-for (var i = 0; i < 9; i++) {
-  numbers[i]=0;
+for (var j = 0; j < 9; j++) {
+  numbers[j]=0;
 }
-
+var tb = new Array();
+for(var i=0; i<9; i++){
+  tb[i]=i;
+}
 function tictactoe(t) {
   if (numbers[t - 1] == 0) {
     if (n % 2 == 0) {
@@ -14,5 +17,33 @@ function tictactoe(t) {
       numbers[t - 1] = 1;
     }
     n--;
+  }
+}
+function win(u){
+  var win=0;
+  var t = document.querySelector('#td'+u);
+  if(t.classList.contains('p1')){
+    tb[u-1]=11;
+  }else if(t.classList.contains('p2')){
+    tb[u-1]=22;
+  }
+  for(i=0; i<3; i++){
+    if(tb[3*i]===tb[3*i+1]&&tb[3*i]===tb[3*i+2]){
+      win=tb[3*i];
+    }else if(tb[i]===tb[i+3]&&tb[i]===tb[i+6]){
+      win=tb[i];
+    }else if(i<2){
+      if(tb[2*i]===tb[4]&&tb[2*i]===tb[8-(2*i)]){
+        win=tb[2*i];
+      }
+    }
+  }
+  console.log(win);
+  if(win>0){
+    if(win===11){
+      alert("Player1 win!!");
+    }else{
+      alert("Player2 win!!");
+    }
   }
 }
